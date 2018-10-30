4070dc39ca261d62868a91f4e1e62de309d3adf0
Patrick A Kim <pkim050@gmail.com>
2018-10-30 22:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ var funkyFunction = (function() {
     console.log("Inner function has been called.");
     return "FUNKY!";
   }
-})();
+}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-funkyFunction;
-var theFunk = funkyFunction();
+var theFunk = funkyFunction;
+theFunk();
